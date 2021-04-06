bc3558c43d6c9ff1fbd520159fc4927c1088712c
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-06 11:29:27 -0500

    adding in search queries and beginning variable structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
+var platformCode={
+	Playstation4:"18",
+	Playstation3:"16",
+	Xboxone:"1",
+	Xbox360:"14",
+	Switch:"7",
+	Nintendo3ds:"8",
+	PC:"4",
+}
+console.log(platformCode);
+
 const settings = {
+	// Platform for PC code is 4, ps4 is 18, xbox one 1, ps3 is 16, 360 is 14, switch is 7, 3ds is 8
 	"async": true,
 	"crossDomain": true,
-	"url": "https://rawg-video-games-database.p.rapidapi.com/games?page_size=20&ordering=metacritic",
+	"url": `https://rawg-video-games-database.p.rapidapi.com/games?genres=${genreSelection}&platforms=${platformCode}&ordering=metacritic&page_size=20`,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "71e3147708msh713981020d02028p1c2586jsn77d951e6a61b",
