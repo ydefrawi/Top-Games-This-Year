db28c1c454c5eda0cccef923b0102a480ec9bc50
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-08 14:31:50 -0500

    adding stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,82 +7,73 @@ var year = moment().format("YYYY");
 var pastYear = moment().subtract(1, 'Y').format('YYYY');
 
 
-//To find the VALUE of an item in an object, use [objectname].[key]    
-//The line below returns "18" (PS4's code)
-platform = platformCodes.PS4;
-//The same will apply to genreSelection object
+function selectGenre(){
+	var genreSelection=$(this).data('genre');
+	console.log(genreSelection);
+};
 
-
-//We might need a function converting whatever the user selects in the drop down
-//to the correct 'value' before we pass it into the API url. Might be simpler for Genre(?)
-//Since they keys are sequential starting from 1 there could be an easy solution.   
-
-var genreSelection=
-var platformSelection=
-
-// console.log("Genre Object:")
-// console.log(genreSelections);
-
-
-//${platform} below will need to be swapped out with a variable containing the actual platform code (84 or whatever).
-//before it was concatenating in the entire object. 
+function selectPlatform(){
+	var platformSelection=$(this).data('console')
+	var platformSelection=JSON.stringify(platformSelection);
+	console.log(platformSelection);
+};
 
 
 //I guess this section has to global?? 
-const settings = {
-	"async": true,
-	"crossDomain": true,
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
 
-	"url": `https://rawg-video-games-database.p.rapidapi.com/games?dates=${year},${pastYear}&genres=${genreSelection}&platforms=${platformSelection}&page_size=10`,
+// 	"url": `https://rawg-video-games-database.p.rapidapi.com/games?dates=${year},${pastYear}&genres=${genreSelection}&platforms=${platformSelection}&page_size=10`,
 
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "71e3147708msh713981020d02028p1c2586jsn77d951e6a61b",
-		"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com"
-	}
-};
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "71e3147708msh713981020d02028p1c2586jsn77d951e6a61b",
+// 		"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com"
+// 	}
+// };
 
 
 //FUNCTIONS--------------------------------
 
 
 //Accepts selections from user to retrieve response. Calls the rendering function(s)
-var getRAWGData = function () {
-	$.ajax(settings).done(function (response) {
+// var getRAWGData = function () {
+// 	$.ajax(settings).done(function (response) {
 
-		console.log(response.results);
-		renderGameData(response)
+// 		console.log(response.results);
+// 		renderGameData(response)
 
 
-	});
-}
+// 	});
+// }
 
 // Renders game data to the page
-var renderGameData = function (response, genre, platform) {
+// var renderGameData = function (response, genre, platform) {
 
-	for (i = 0; i < 20; i++) {
+// 	for (i = 0; i < 20; i++) {
 
-		//creates div for a 'section' for each game 
-		var gameSection = document.createElement('div')
-		gameSection.textContent = "Game Name"
-		gameNameEl.appendChild(gameSection)
+// 		//creates div for a 'section' for each game 
+// 		var gameSection = document.createElement('div')
+// 		gameSection.textContent = "Game Name"
+// 		gameNameEl.appendChild(gameSection)
 
 
-		gameAPI = response.results[i].name
-		console.log(gameAPI)
-		var gameName = document.createElement('h4');
-		gameName.textContent = gameAPI
-		gameSection.appendChild(gameName);
+// 		gameAPI = response.results[i].name
+// 		console.log(gameAPI)
+// 		var gameName = document.createElement('h4');
+// 		gameName.textContent = gameAPI
+// 		gameSection.appendChild(gameName);
 
-		releasedAPI = response.results[i].released;
-		var released = document.createElement('h4');
-		released.textContent = releasedAPI;
-		gameSection.appendChild(released);
+// 		releasedAPI = response.results[i].released;
+// 		var released = document.createElement('h4');
+// 		released.textContent = releasedAPI;
+// 		gameSection.appendChild(released);
 
-		MCscore = response.results[i].metacritic;
-		var metaRating = document.createElement('h4')
-		metaRating.textContent = MCscore;
-		gameSection.appendChild(metaRating)
+// 		MCscore = response.results[i].metacritic;
+// 		var metaRating = document.createElement('h4')
+// 		metaRating.textContent = MCscore;
+// 		gameSection.appendChild(metaRating)
 
 		// for(i=0; i<response.results[i].short_screenshots.length; i++){
 
@@ -96,8 +87,8 @@ var renderGameData = function (response, genre, platform) {
 		//  =
 
 
-	}
-}
+// 	}
+// }
 
 
 
@@ -130,7 +121,7 @@ function gameVideo(gameName) {
 //EVENT LISTENERS----------------------------------------
 
 // this will later be called by an eventListener. Will have 2 arguments, genre and platform
-getRAWGData();
+// getRAWGData();
 $('.dropdown-trigger').dropdown();
 
 // document.addEventListener('DOMContentLoaded', function() {
@@ -144,3 +135,6 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 $('.slider').slider({ width: 1000, });
+
+$(".genreSelection").on("click",selectGenre);
+$(".platformSelection").on("click",selectPlatform);
