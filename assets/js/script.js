10b5f3fafe84c2665ba6f26f4ea39db6229da9ee
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-08 13:43:22 -0500

    removing hrefs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ var gameNameEl = document.querySelector('body')
 var copyrightEl = document.querySelector('#copyright')
 
 
-var year=moment().format("YYYY");
+var year = moment().format("YYYY");
 var pastYear = moment().subtract(1, 'Y').format('YYYY');
-var platformCodes={
-	PS4:"18",
-	PS5:"187",
-	XB1:"1",
-	SWITCH:"7",
-	PC:"4",
+var platformCodes = {
+	PS4: "18",
+	PS5: "187",
+	XB1: "1",
+	SWITCH: "7",
+	PC: "4",
 };
 
 //To find the VALUE of an item in an object, use [objectname].[key]    
@@ -23,17 +23,17 @@ platform = platformCodes.PS4;
 //to the correct 'value' before we pass it into the API url. Might be simpler for Genre(?)
 //Since they keys are sequential starting from 1 there could be an easy solution.   
 
-var genreSelections={
-	1:"action",
-	2:"puzzle",
-	3:"rpg",
-	4:"indie",
-	5:"vr",
-	6:"shooter",
-	7:"fps",
-	8:"sports",
-	9:"survival",
-	10:"horror",
+var genreSelections = {
+	1: "action",
+	2: "puzzle",
+	3: "rpg",
+	4: "indie",
+	5: "vr",
+	6: "shooter",
+	7: "fps",
+	8: "sports",
+	9: "survival",
+	10: "horror",
 }
 
 // console.log("Genre Object:")
@@ -49,7 +49,7 @@ const settings = {
 	"async": true,
 	"crossDomain": true,
 
-	"url": `https://rawg-video-games-database.p.rapidapi.com/games?dates=2021&genres=action&platforms=${platform}&page_size=20`,
+	"url": `https://rawg-video-games-database.p.rapidapi.com/games?dates=${year},${pastYear}&genres=action&platforms=${platform}&page_size=10`,
 
 	"method": "GET",
 	"headers": {
@@ -63,22 +63,21 @@ const settings = {
 
 
 //Accepts selections from user to retrieve response. Calls the rendering function(s)
-var getRAWGData=function(){
-$.ajax(settings).done(function (response) {
+var getRAWGData = function () {
+	$.ajax(settings).done(function (response) {
 
-	console.log(response.results);
-	renderGameData(response)
-	
+		console.log(response.results);
+		renderGameData(response)
 
-});
+
+	});
 }
 
 // Renders game data to the page
-var renderGameData = function(response, genre, platform){
+var renderGameData = function (response, genre, platform) {
 
-	for (i=0; i<20; i++)
-	{
-		
+	for (i = 0; i < 20; i++) {
+
 		//creates div for a 'section' for each game 
 		var gameSection = document.createElement('div')
 		gameSection.textContent = "Game Name"
@@ -92,27 +91,27 @@ var renderGameData = function(response, genre, platform){
 		gameSection.appendChild(gameName);
 
 		releasedAPI = response.results[i].released;
-		var released=document.createElement('h4');
-		released.textContent=releasedAPI; 
+		var released = document.createElement('h4');
+		released.textContent = releasedAPI;
 		gameSection.appendChild(released);
 
-		MCscore = response.results[i].metacritic; 
+		MCscore = response.results[i].metacritic;
 		var metaRating = document.createElement('h4')
-		metaRating.textContent=MCscore;
+		metaRating.textContent = MCscore;
 		gameSection.appendChild(metaRating)
 
 		// for(i=0; i<response.results[i].short_screenshots.length; i++){
 
 		// }
-	
+
 		// screenshotsArr = response.results[i].metacritic; 
 		// var metaRating = document.createElement('h4')
 		// metaRating.textContent=MCscore;
 		// gameSection.appendChild(metaRating)
-		
+
 		//  =
 
-		
+
 	}
 }
 
@@ -129,18 +128,18 @@ var renderGameData = function(response, genre, platform){
 
 // when a game is selected it passes the game name into the search query for youtube
 function gameVideo(gameName) {
-    fetch(`https://www.googleapis.com/youtube/v3/search?list=${gameName}&key=AIzaSyAeKSxi1SCBtErDfdLze16qiqmLbnGTMxI`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-        // catches any fetch errors
-        .catch(function (err) {
-            console.log(err);
-        });
-	};
+	fetch(`https://www.googleapis.com/youtube/v3/search?list=${gameName}&key=AIzaSyAeKSxi1SCBtErDfdLze16qiqmLbnGTMxI`)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+		})
+		// catches any fetch errors
+		.catch(function (err) {
+			console.log(err);
+		});
+};
 
 
 
@@ -155,9 +154,9 @@ $('.dropdown-trigger').dropdown();
 //     var instances = M.Slider.init(elems, options);
 //   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+	var elems = document.querySelectorAll('.carousel');
+	var instances = M.Carousel.init(elems, options);
+});
 
-$('.slider').slider({width:1000,});
+$('.slider').slider({ width: 1000, });
